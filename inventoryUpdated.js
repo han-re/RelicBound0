91578c03d0f94f36e032e146a4d73d24c7939ba4
Ryan-alex5 <ryan.alex.shijee@gmail.com>
2025-04-03 00:33:33 +0530

    Add files via upload

---
+++ inventoryUpdated.js
@@ -0,0 +1,132 @@
+function addToInventory(itemSrc){
+    let inventory = document.getElementById("inventory-items");
+
+    let item = document.createElement("img");
+    item.src = itemSrc;                                                 //This function adds the inventory image to the bar.
+    item.alt = "Inventory Item";
+    item.style.width = "50px";
+    item.style.height = "50px";
+    item.style.border = "2px solid white";
+    item.style.borderRadius = "5px";
+
+    inventory.appendChild(item);
+}
+
+ //This gets the userId from the session storage
+ let userId = sessionStorage.getItem("userId");
+
+
+
+ //This is to obtain the userInvId and store it in the userInvId variable below.
+         async function fetchuserInvId(userId){
+ 
+         let hostname = "localhost";
+         let username = "ralex01";
+         let password = "PVbhXXLBnWZY23x8";
+         let database = "CSC1034_CW_47";
+         let query = `SELECT userInvId FROM user_inventoryTable WHERE userId = ${userId}  LIMIT 1`;
+ 
+         let params = new URLSearchParams();
+             params.append('hostname', hostname);
+             params.append('username', username);
+             params.append('password', password);
+             params.append('database', database);
+             params.append('query', query);
+ 
+             let url = 'https://ralex01.webhosting1.eeecs.qub.ac.uk/dbConnector.php';
+             try {
+                 let response = await fetch(url, {
+                     method: 'POST',
+                     headers: {"Content-Type":"application/x-www-form-urlencoded"},
+                     body: params
+                 }); // Waits for a response from the fetch
+ 
+                 if(!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+ 
+ 
+                 let result = await response.json(); // Waits for the JSON to be parsed
+ 
+                 if(result.data && result.data.length > 0){
+                     userInvId = result.data[0].userInvId;
+                     console.log("Fetched userInvId ID: ", userInvId);
+                     return userInvId;
+                 }else{
+                     console.log("No userInvId found for the given user.")
+                 }
+             }catch (error){
+                 console.error("Error fetching userInvId: ", error);
+             }
+ 
+         }
+         async function insertIntoInventory(userId, itemId) {
+             let userInvId = await fetchuserInvId(userId);
+             if(!userInvId){
+                 console.error("Cannot insert item.");
+                 return;
+             }
+ 
+             
+             console.log("new ID", userInvId)  
+             //userInvId is correctly stored in userInvId
+             
+             
+             //This part executes the INSERT query. It will insert the item into the database
+         let hostname = "localhost";
+         let username = "ralex01";
+         let password = "PVbhXXLBnWZY23x8";
+         let database = "CSC1034_CW_47";
+         let query = `INSERT INTO inventoryTable (itemId, userInvId) VALUES ( 2, ${userInvId})`;
+ 
+         let params = new URLSearchParams();
+             params.append('hostname', hostname);
+             params.append('username', username);
+             params.append('password', password);
+             params.append('database', database);
+             params.append('query', query);
+ 
+             console.log(params.toString());
+ 
+             executeDatabaseQuery(params);
+         
+         async function executeDatabaseQuery(params) {
+             let url = 'https://ralex01.webhosting1.eeecs.qub.ac.uk/dbConnector.php';
+             try {
+                 let response = await fetch(url, {
+                     method: 'POST',
+                     headers: {"Content-Type":"application/x-www-form-urlencoded"},
+                     body: params
+                 }); // Waits for a response from the fetch
+ 
+                 if(!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+ 
+ 
+                 let result = await response.json(); // Waits for the JSON to be parsed
+ 
+                 // If an error has been returned, e.g. incorrect SQL syntax, display the error
+                 if (result.error) {
+                     console.log(result.error.toString());
+                 }
+                 // If a dataset has been returned, e.g. from a SELECT statement, do something.
+                 else{
+                     console.log("Successful");
+                     if(result.affected_rows !== undefined){
+                         console.log(`Affected Rows: ${result.affected_rows}`);
+                     }
+                 }
+                
+                 
+             } catch (error) {
+                 console.error('Error parsing JSON:', error);
+             }
+         }
+ 
+         }
+         //Invoking the function
+         insertIntoInventory(userId,2); //Make sure the itemId is the correct one corresponding to the ItemId table in the database
+ 
+ 
+         
+
+
+
+
